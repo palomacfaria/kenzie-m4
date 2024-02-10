4e7edde822fd02139b6c63fb40b4c0938d093c34
Paloma Cristina Faria <palomacfaria@outlook.com>
2024-02-09 22:09:07 -0300

    Inicio da demo de introducao ao TS

---
+++ Demos/02-introducao/src/index.ts
@@ -0,0 +1,7 @@
+//O Typescript não afeta a funcionalidade da aplicação
+//Sintaxe descritiva: fluxo de dados na aplicação
+
+console.log("olá mundo!");
+
+//Depndências de produção - pacotes essenciais para rodar o projeto no ar
+//Dependências de desenvolvimento - pacotes para auxiliar o desenvolvimento
