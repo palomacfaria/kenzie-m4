af91892d066c4ad510817232040a3fe4dd472335
Paloma Cristina Faria <palomacfaria@outlook.com>
2024-02-29 23:17:33 -0300

    Crud com express

--- Canvas/sprint-3/02-atividade/src/index.ts
+++ Canvas/sprint-3/02-atividade/src/index.ts
@@ -5,22 +5,27 @@ const app = express();
 app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Leitura realizada com sucesso!");
+  res.send("Leitura efetuada com sucesso");
 });
 
 app.post("/", (req: Request, res: Response) => {
-  res.send("Criação realizada com sucesso");
+  res.send("Criação efetuada com sucesso");
+});
+
+app.patch("/", (req: Request, res: Response) => {
+  res.send("Atualização parcial efetuada com sucesso");
 });
 
 app.put("/", (req: Request, res: Response) => {
-  res.send("Atualização realizada com sucesso");
+  res.send("Atualização efetuada com sucesso");
 });
 
 app.delete("/", (req: Request, res: Response) => {
-  res.send("Exclusão realizada com sucesso");
+  res.send("Exclusão efetuada com sucesso");
 });
 
 const port = 3000;
-app.listen(port, () => {
+
+app.listen(3000, () => {
   console.log(`API sucessfully started in port ${port}`);
 });
