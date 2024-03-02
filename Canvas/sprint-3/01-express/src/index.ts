46f0ca803b3373b68dbe13c7fa449fee3baf8872
Paloma Cristina Faria <palomacfaria@outlook.com>
2024-03-02 00:34:26 -0300

    request and response

--- Canvas/sprint-3/01-express/src/index.ts
+++ Canvas/sprint-3/01-express/src/index.ts
@@ -5,22 +5,28 @@ const app = express();
 app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Leitura realizada com sucesso!");
+  return res.send("Leitura realizada com sucesso!");
 });
 
 app.post("/", (req: Request, res: Response) => {
-  res.send("Criação realizada com sucesso");
+  //Req - Requisição
+  // console.log(req.body); - corpo da requisição
+  // console.log(req.params); - Parâmetros de url (url/param)
+  // console.log(req.query); Parâmetros de busca (url?param=value)
+  // console.log(req.headers); - Configuraçãoes e credênciais de acesso
+
+  return res.status(201).json({message: "Criação realizada com sucesso"});
 });
 
 app.put("/", (req: Request, res: Response) => {
-  res.send("Atualização realizada com sucesso");
+  return res.send("Atualização realizada com sucesso");
 });
 
 app.delete("/", (req: Request, res: Response) => {
-  res.send("Exclusão realizada com sucesso");
+  return res.send("Exclusão realizada com sucesso");
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`API sucessfully started in port ${port}`);
+  return console.log(`API sucessfully started in port ${port}`);
 });
